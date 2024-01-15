f592b81c7024f47202e2c644f43042657a60cff6
AK-codingmaniac <anshaj13.ugcs20@iiitranchi.ac.in>
2024-01-15 13:20:35 +0530

    Added basic approach using combined state for handling form data

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
 
-//This is a basic approach for handling form data
+//This is a basic approach using combined state for handling form data
 export default function Login() {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredPassword, setEnteredPassword] = useState("");
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("hello" + enteredEmail);
+    console.log("hello " + enteredValues.email);
   }
 
-  function handleEmailChange(event) {
-    setEnteredEmail(event.target.value);
-  }
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
 
-  function handlePasswordChange(event) {
-    setEnteredPassword(event.target.value);
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -29,8 +40,10 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            onChange={handleEmailChange}
-            value={enteredEmail}
+            // onChange={handleEmailChange}
+            // value={enteredEmail}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
           />
         </div>
 
@@ -40,8 +53,12 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            onChange={handlePasswordChange}
-            value={enteredPassword}
+            // onChange={handlePasswordChange}
+            // value={enteredPassword}
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
           />
         </div>
       </div>
